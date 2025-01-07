3f6ac8143b4bf0329ced8d0f1ce17a5c2448db0c
asahtik <asahtik@gmail.com>
2025-01-07 20:44:45 +0100

    Generate compile commands for clangd

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
+#include <iostream>
+
 #include "depthai/depthai.hpp"
 #include "opencv2/opencv.hpp"
 
 int main(int argc, char** argv) {
     dai::Pipeline pipeline;
     auto camRgb = pipeline.create<dai::node::Camera>()->build();
-    auto* output = camRgb->requestOutput(std::make_pair(640, 480));
+    auto* output = camRgb->requestOutput({640, 480});
     if(output == nullptr) {
         std::cout << "Error creating output, exiting\n";
         return 1;
@@ -14,7 +16,8 @@ int main(int argc, char** argv) {
 
     while(pipeline.isRunning()) {
         auto imgFrame = outputQueue->get<dai::ImgFrame>();
-        cv::imshow("rgb", imgFrame->getCvFrame());
+        auto cvFrame = imgFrame->getCvFrame();
+        cv::imshow("rgb", cvFrame);
         int key = cv::waitKey(1);
         if(key == 'q' || key == 'Q') {
             pipeline.stop();
